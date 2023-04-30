ab7ee383c895552fe05a3a9cf03fa60ce9264163
suchdevdarshan7 <122098756+suchdevdarshan7@users.noreply.github.com>
2023-04-30 18:09:53 +0530

    Add files via upload

---
+++ assingment1.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+     
+int main() {
+    // 1. product of two numbers ;
+    
+    /* int a, b ;
+    cout<<" Enter two number ";
+    cin>>a ; 
+    cin>>b ; 
+    cout<<a*b;
+    */
+
+//    2. ASCII VALUE OF U :     
+    /*
+    char ch = 'U';
+    int a = ch  ;
+    cout<<a ; 
+    */
+   
+// 3. Display the area of rectangle by input length and breadth 
+
+    /*
+    int length,breadth ; 
+    cin>>length ; 
+    cin>>breadth ; 
+    cout<<length * breadth ; 
+    */
+    
+// 4. Find the size of the datatypes of the all the datatypes
+    /*
+    int a;
+    short e;
+    long r;
+    double d;
+    float b ;
+    bool y ; 
+    char ch ; 
+
+
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(e)<<endl;
+    cout<<sizeof(ch)<<endl;
+    cout<<sizeof(r)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(y)<<endl;
+    */
+
+// 5. Swap two numbers with a third number ; 
+/*    
+    int a = 19 ; 
+    int b = 39 ; 
+    int temp; 
+
+    temp = a ; 
+    a = b ; 
+    b = temp ; 
+    cout<<a<<endl;
+     cout<<b<<endl; 
+*/
+
+// 6.Cube of a number 
+  
+  /*
+    int num ;
+    cin>>num ;
+    cout<<num*num*num;
+*/
+
+}
