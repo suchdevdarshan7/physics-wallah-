4e65b95a468a4a0f62ac49933f3fc39ff6f7616a
suchdevdarshan7 <122098756+suchdevdarshan7@users.noreply.github.com>
2023-05-02 20:58:54 +0530

    Add files via upload

---
+++ assingment2.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1. To find whether to number are equal or not
+
+    /*
+    int a,b,result;
+    cout<<"Enter the number :";
+    cin>>a;
+    cin>>b;
+    result =  (a==b) ? 1 : 0;
+    cout<<result;
+    */
+
+    // 2. To find whether two number is greater less than 50 and less than b ;
+   /*
+    int a, b, result;
+    cout << "Enter the number :";
+    cin >> a;
+    cin >> b;
+
+    result = (a<50) && (a<b) ? 1 : 0 ;
+    cout<<result;     
+    */
+
+    // 3. To find the number of girls who scored A grade 
+
+    // int boys = 17,girls, total ; 
+    // int result ; 
+    // total = (80*45 )/100;
+    // result = total - boys ;  
+    // cout<<result ; 
+
+    // 4. Sum of the numbers from the 1 st and second last digit 
+
+    // int num =12345,sum ;
+    // int temp = num ; 
+    // int first = num /10000;
+    // num = num %10000;
+    // int last = (num /10)%10;
+    // sum = first + last ; 
+    // cout<<sum ; 
+
+    // 5. Sum all the number in a number 
+    
+    // int num = 123;
+    // int first = num/100;
+    // int second = (num/10)%10 ; 
+    // int last = num%10 ;
+    // int sum = first + second + last ; 
+    // cout<<sum ;
+    
+    // 6. Calculator using + - * / 
+
+    // float num1 ,num2;
+    // cout<<"Enter two number "<<endl; 
+    // cin>>num1; 
+    // cin>>num2;
+    // cout<<" Sum "<<num1+num2 <<endl ;  
+    // cout<<" Difference "<<num1-num2 <<endl ;  
+    // cout<<" Multiply "<<num1*num2 <<endl ;  
+    // cout<<" Divide "<<num1/num2 <<endl ;  
+
+}
